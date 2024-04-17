fff468c7ae38792cf24b45935b06bbd0139143af
Santiago Cuevas <santiagocuevas77@gmail.com>
2024-04-16 19:24:45 -0600

    add create book feature

--- script.js
+++ script.js
@@ -12,11 +12,10 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 304, true);
-
 addBookToLibrary("The Three Body Problem", "Liu Cixin", 400, false);
-
 addBookToLibrary("The Martian", "Andy Weir", 480, true);
 
+// Render Books already in library
 for (i = 0; i < myLibrary.length; i++) {
   const bookGrid = document.querySelector(".book-grid");
   const bookCard = document.createElement("div");
@@ -31,6 +30,7 @@ for (i = 0; i < myLibrary.length; i++) {
   bookGrid.appendChild(bookCard);
 }
 
+// Add book modal window
 const modal = document.querySelector("dialog");
 const addBookButton = document.querySelector(".add-book");
 const closeModalBtn = document.querySelector(".close-btn");
@@ -42,3 +42,39 @@ addBookButton.addEventListener("click", () => {
 closeModalBtn.addEventListener("click", () => {
   modal.close();
 });
+
+// Handle form submit
+const addBookForm = document.addBook;
+
+addBookForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const titleInput = document.getElementById("title").value;
+  const authorInput = document.getElementById("author").value;
+  const pagesInput = document.getElementById("pages").value;
+  const readInput = document.getElementById("read");
+
+  // Create book
+  addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
+
+  // Render book
+  addBookCard(myLibrary[myLibrary.length - 1]);
+
+  console.log(myLibrary);
+  modal.close();
+  addBookForm.reset();
+});
+
+// Render single book function
+function addBookCard(book) {
+  const bookGrid = document.querySelector(".book-grid");
+  const bookCard = document.createElement("div");
+
+  bookCard.classList.add("book-card");
+  bookCard.innerHTML = `
+  <h2 class="white-text title">${book.title}</h2>
+  <p class="white-text author">${book.author}</p>
+  <p class="white-text">Pages: ${book.pages}</p>
+  <p class="white-text">${book.read ? "Read" : "Unread"}</p>
+  `;
+  bookGrid.appendChild(bookCard);
+}
